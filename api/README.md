9e8ce59fbb5e239c13b602fdcb20d02edce7551b
edwardanderson <anderson.edw@gmail.com>
2021-06-27 09:36:50 +0200

    init

---
+++ api/README.md
@@ -0,0 +1,15 @@
+# Glitch Application
+
+Resolve an Also Known As identifier:
+
+```bash
+curl -L https://alsoknownas.glitch.me/beleedf-lauw-tekening-ding
+```
+
+Resolve a URI to Also Known As identifier:
+
+```
+curl -X POST https://alsoknownas.glitch.me/ \
+    --header "Content-Type: application/json" \
+    --data '{"uri": "https://pid.uba.uva.nl/ark:/88238/b1990032751780205131"}'
+```
---
+++ api/server.py
@@ -0,0 +1,47 @@
+'''
+Resolve an AKA identifier to a location on the web.
+'''
+
+
+import json
+from flask import Flask, request, redirect, abort
+from pathlib import Path
+
+
+app = Flask(__name__)
+
+@app.route('/<identifier>', methods=['GET'])
+@app.route('/aka2uri/<identifier>', methods=['GET'])
+def resolve(identifier=None):
+    '''
+    Resolve an AKA identifier.
+    '''
+    try:
+        target = mapping[identifier]
+        return redirect(target, code=302)    
+    except KeyError:
+        return abort(404)
+
+
+@app.route('/', methods=['POST'])
+@app.route('/uri2aka', methods=['POST'])
+def resolve_uri():
+    try:
+        uri = request.json['uri']
+        aks_id = mapping[uri]
+        return aks_id
+    exception Exception:
+        return abort(404)
+
+
+if __name__ == '__main__':
+    mapping = {}
+    for mapping_file in Path('.').glob('mapping*.json'):
+        print(mapping_file)
+        with open(str(mapping_file)) as data:
+            local_mapping = json.load(data)
+            for key in local_mapping:
+                value = local_mapping[key]
+                mapping[key] = value
+                mapping[value] = key
+    app.run()

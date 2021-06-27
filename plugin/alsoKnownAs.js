932581d130d84946c953355ec538cf90e650d2b4
edwardanderson <anderson.edw@gmail.com>
2021-06-27 13:36:52 +0200

    show string in corner

--- plugin/alsoKnownAs.js
+++ plugin/alsoKnownAs.js
@@ -1,1 +1,26 @@
-document.body.style.border = "5px solid red";
+const userAction = async () => {
+    const response = await fetch('https://alsoknownas.glitch.me/', {
+      method: 'POST',
+      body: {
+          'uri': document.location.href
+      }, // string or object
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/ld+json'
+      }
+    });
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+    console.log("test!!!")
+    console.log(myJson)
+    const identifier = myJson['identifies'][0]['id'];
+
+  }
+  
+
+var header = document.createElement('p');
+header.textContent = 'this is where the identifier should go';
+document.body.appendChild(header);
+header.style.position = 'absolute';
+header.style.bottom = '50px';
+header.style.right = '50px';

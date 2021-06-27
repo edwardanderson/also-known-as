44b0f32ad35b677a4171af0307d22991c77e3866
edwardanderson <anderson.edw@gmail.com>
2021-06-27 10:14:16 +0200

    refactor

--- api/server.py
+++ api/server.py
@@ -6,7 +6,6 @@ Resolve an AKA identifier to a location on the web.
 import json
 from flask import Flask, request, redirect, abort, jsonify
 from pathlib import Path
-from werkzeug.exceptions import NotAcceptable
 
 
 app = Flask(__name__)
@@ -25,25 +24,7 @@ def resolve(identifier=None):
     
     preferred_content = content_negotiation()
     if preferred_content in ['application/ld+json', 'application/json']:
-        linked_art_object = {
-            "@context": "https://linked.art/ns/v1/linked-art.json",
-            "id": f"https://alsoknownas.glitch.me/{identifier}",
-            "type": "Identifier",
-            "content": identifier,
-            "identifies": [
-                {
-                    "id": target,
-                    "type": "HumanMadeObject"
-                }
-            ],
-            "classified_as": [
-                {
-                    "id": "http://vocab.getty.edu/aat/300435704",
-                    "type": "Type",
-                    "_label": "record identifiers"
-                }
-            ]
-        }
+        linked_art_object = serialise_to_linked_art(identifier, target)
         return jsonify(linked_art_object)
     else:
         return redirect(target, code=302)  
@@ -60,9 +41,8 @@ def resolve_uri():
 
     preferred_content = content_negotiation()
     if preferred_content == 'application/ld+json':
-        linked_art_object = {
-          
-        }
+        # Implementation required?
+        pass
     else:
       return aks_id
       
@@ -78,6 +58,29 @@ def content_negotiation():
         raise NotAcceptable(description)
     return preferred_content
 
+
+def serialise_to_linked_art(identifier, target):
+    linked_art_object = {
+        "@context": "https://linked.art/ns/v1/linked-art.json",
+        "id": f"https://alsoknownas.glitch.me/{identifier}",
+        "type": "Identifier",
+        "content": identifier,
+        "identifies": [
+            {
+                "id": target,
+                "type": "HumanMadeObject"
+            }
+        ],
+        "classified_as": [
+            {
+                "id": "http://vocab.getty.edu/aat/300435704",
+                "type": "Type",
+                "_label": "record identifiers"
+            }
+        ]
+    }
+    return linked_art_object
+  
 
 if __name__ == '__main__':
     mapping = {}
